ed6293299f34c67d7f429504e10dec8d5c9f2de3
Petro Silenius <petro.silenius@gmail.com>
2023-02-26 15:26:00 +0200

    Add cors handling to the API

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import router from './router';
 import morgan from 'morgan';
+import cors from 'cors';
 import { protect } from './modules/auth';
 import { createUser, signIn } from './handlers/user';
 
 const app = express();
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
