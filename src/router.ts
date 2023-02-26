e878b0a6e40d7c151c020f0d99015f413010cb22
Petro Silenius <petro.silenius@gmail.com>
2023-02-26 18:33:38 +0200

    Add validation for POST and PUT operations with express-validator

--- src/router.ts
+++ src/router.ts
@@ -1,27 +1,57 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
+import { handleInputErrors } from './modules/middlewares';
 
 const router = Router();
 
 router.get('/products', (req, res) => {
   res.json({ message: 'Hello World!' });
 });
-router.post('/product', () => {});
+router.post('/product', body('name').isString(), handleInputErrors, (req, res) => {});
 router.get('/product/:id', () => {});
-router.put('/product/:id', () => {});
+router.put('/product/:id', body('name').isString(), handleInputErrors, (req, res) => {});
 router.delete('/product/:id', () => {});
 
 router.get('/product/:productId/updates', (req, res) => {
   res.json({ message: 'Hello World!' });
 });
-router.post('/product/:productId/updates', () => {});
+router.post(
+  '/product/:productId/updates',
+  body('title').isString(),
+  body('content').isString(),
+  body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']),
+  body('version').optional().isString(),
+  handleInputErrors,
+  () => {}
+);
 router.get('/product/:productId/update/:id', () => {});
-router.put('/product/:productId/update/:id', () => {});
+router.put(
+  '/product/:productId/update/:id',
+  body('title').isString(),
+  body('content').isString(),
+  body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']),
+  body('version').optional().isString(),
+  handleInputErrors,
+  () => {}
+);
 router.delete('/product/:productId/update/:id', () => {});
 
 router.get('/product/:productId/update/:id/updatePoints', () => {});
-router.post('/product/:productId/update/:id/updatePoint', () => {});
+router.post(
+  '/product/:productId/update/:id/updatePoint',
+  body('name').isString(),
+  body('description').isString(),
+  handleInputErrors,
+  () => {}
+);
 router.get('/product/:productId/update/:id/updatePoint/:id', () => {});
-router.put('/product/:productId/update/:id/updatePoint/:id', () => {});
+router.put(
+  '/product/:productId/update/:id/updatePoint/:id',
+  body('name').optional().isString(),
+  body('description').optional().isString(),
+  handleInputErrors,
+  () => {}
+);
 router.delete('/product/:productId/update/:id/updatePoint/:id', () => {});
 
 export default router;
